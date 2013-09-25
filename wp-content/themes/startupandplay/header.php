c410a0d1cc7229370c0c038ca2647ba7c727a38d
Patrick Shampine <patrick@patrickshampine.com>
2013-09-25 13:09:44 -0700

    Removed zoom (user-scalable)

--- wp-content/themes/startupandplay/header.php
+++ wp-content/themes/startupandplay/header.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title><?php wp_title(); ?></title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
 <?php wp_head(); ?>
 </head>    
 <body <?php body_class(); ?>>
