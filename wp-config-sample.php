a02ccfab95ca057654b36190eefdb6912c12f353
Patrick Shampine <patrick@patrickshampine.com>
2013-09-29 22:22:12 -0700

    Replaced sample config with WordPress sample

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'startupandplay_com');
+define('DB_NAME', 'database_name_here');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'username_here');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', 'password_here');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -33,14 +33,6 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-/** Update Site URLs **/
-define('WP_HOME','http://startupandplay.dev');
-define('WP_SITEURL','http://startupandplay.dev');
-
-/** Stop CF7 from loading on every page */
-define('WPCF7_LOAD_JS', false);
-define('WPCF7_LOAD_CSS', false);
-
 /**#@+
  * Authentication Unique Keys and Salts.
  *
@@ -50,14 +42,14 @@ define('WPCF7_LOAD_CSS', false);
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'qR45qUr(Y? 4b B}vKCvOO]v-w@} 4pN;m/&H4>5&#!51o+!CU35^@HU9,T-Epff');
-define('SECURE_AUTH_KEY',  ' ~bWyV+-Ok#,@9zHic/uv%>;L8d-zduV7ee]TCp9C-ex.|$SyPy{Z|^ZuO?je4b9');
-define('LOGGED_IN_KEY',    'nB]+YWPd+{hUuMgQ_|ci[ZD|.L|P0 #:i-b!YfT]SqWRNdVBuUsD pB(+=S/+Oy#');
-define('NONCE_KEY',        'DwlVM/R0pQ~c.+cm`^yHskKF!wfcIu~h4Tc(zk.1rLpX>U6?4Sgn/2KcxO-u1[C{');
-define('AUTH_SALT',        ']ns&izIfgckC|qv53cm8z*-5Q`Oru}|@};-tN n+vRkG#EYIYFgqY-=Ir`Yvt1S_');
-define('SECURE_AUTH_SALT', '8u<P&nX4r>bU@BiL1NB-+fu-NOZnkQPdUQJb;%>2zw#2`csoqe0VP_`mD@gS=R-$');
-define('LOGGED_IN_SALT',   '1W*o82Lg?%j0IOx{QkP0]C}gLl`#+Ox)+~08b;P@%^ -P<{dR=X]W0VaIVJ36_l?');
-define('NONCE_SALT',       '9o^7HO4r-qBZwXDtje_5Ul`F1aXg9?;!Edq{o:|/O8&/putCVun J|9@v,Q0D}[i');
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
 
 /**#@-*/
 
@@ -67,13 +59,7 @@ define('NONCE_SALT',       '9o^7HO4r-qBZwXDtje_5Ul`F1aXg9?;!Edq{o:|/O8&/putCVun 
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_42qdg8_';
-
-/**
-* Limits total Post Revisions saved per Post/Page.
-* Change or comment this line out if you would like to increase or remove the limit.
-*/
-define('WP_POST_REVISIONS',  false);
+$table_prefix  = 'wp_';
 
 /**
  * WordPress Localized Language, defaults to English.
