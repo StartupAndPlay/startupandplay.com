41549bf37b13e6ab8bb12eabf7a495b49ad32169
Patrick Shampine <patrick@patrickshampine.com>
2013-10-04 18:22:42 -0700

    Missing }

--- wp-content/themes/startupandplay/functions.php
+++ wp-content/themes/startupandplay/functions.php
@@ -97,9 +97,9 @@ _gaq.push([\'_trackPageview\']);
 var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
 ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
 var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
-})();
+})();}
 </script>';}
-add_action('wp_head','google_analytics');
+//add_action('wp_head','google_analytics');
 
 // Removes ul class from wp_nav_menu
 function remove_ul ( $menu ){
